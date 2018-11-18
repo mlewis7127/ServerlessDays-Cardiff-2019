b197f92eb04264785c2d70b9ceb2fe77108e811e
Matt Lewis <matthew.lewis@dvla.gsi.gov.uk>
2018-11-18 21:20:24 +0000

    Add abstract for Yan Cui keynote

--- src/talks.js
+++ src/talks.js
@@ -5,7 +5,7 @@ module.exports = [
     photoUrl: "yan-cui.jpg",
     title: "Serverless - more FinDev than DevOps",
     speaker: "Yan Cui",
-    abstract: `This talk covers how serverless is driving out a new set of architectural practices. Abstract coming soon`
+    abstract: `A lot of the discussions around serverless has been about the benefits it brings to the table with regards to DevOps - more infrastructure automation, scalability and resilience out-of-the-box. Developers love it because they can offload even more undifferentiated heavy-lifting to their cloud vendors, and they can focus their energy on building the things their users want. Businesses benefit hugely too because they have happier developers who can deliver value faster!\nBut the true power of the serverless paradigm, for the business, is the pay-per-invocation model. It allows them to finally understand the cost of user transactions, and calculate the return on investment of features. And if you embrace this superpower then it can even open the door to an entirely new business model built around pay-per-transaction and give your business the competitive advantage over your rivals.`
   },
   {
     key: "ian-massingham",
