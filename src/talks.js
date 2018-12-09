b01c1b0042beb551682afa28e007a2dc8984d1d5
Matt Lewis <matthew.lewis@dvla.gsi.gov.uk>
2018-12-09 20:46:42 +0000

    Add talks and speaker images to date

--- src/talks.js
+++ src/talks.js
@@ -23,4 +23,68 @@ module.exports = [
     speaker: "Simona Cotin",
     abstract: `Current trends in software and backend architecture is evolving towards a more loosely coupled more granular design. The latest development on that front is the advent of serverless which allows you to run applications in very cost-effective ephemeral services. This is why it is important to have a proper gateway for your API that is able to route all your requests to the designated endpoint. GraphQL stands out in that respect as being a mature open sourced standard started at Facebook.`
   },
+  {
+    key: "ncsc",
+    bio: "Senior security researcher from NCSC.",
+    photoUrl: "ncsc.jpg",
+    title: "Good Security Made Easier with Serverless",
+    speaker: "NCSC researcher",
+    abstract: `The National Cyber Security Centre (NCSC) has been researching the security properties of serverless components in the public cloud to identify the impact serverless has on security. As the research nears its conclusion, we discuss our thoughts and experiences with serverless technology.`
+  },
+  {
+    key: "olga-skobeleva",
+    bio: "Olga Skobeleva helps build the future of the Internet as a Solutions Engineer at Cloudflare. She developed her first website at 8 years old which was about an anime called Sailor Moon. Before getting a computer science degree in Finland she also studied law including cybercrime. Her web development experience and passion for law led her to a career as a security network engineer.\n During her CS studies, she got the top female score in the Cisco Networking Academy 2014 CCNA NetRiders Skills Competition for Northern Europe. Along with bringing her expertise on web security and performance, she became a technical customer advocate within Cloudflare, ensuring customers’ success on the Cloudflare platform.",
+    photoUrl: "olga-skobeleva.jpg",
+    title: "Security: the Serverless Future",
+    speaker: "Olga Skobeleva",
+    abstract: `Does security have to come at a cost of performance and maintenance? Your security is only as strong as the weakest human link and their patience and diligence in following proper protocols. Serverless computing is the future of how complex software systems and their security will be designed and built. This talk will demonstrate several migration cases that Serverless technology can already help with today.\nSome modern hosting platforms don’t give you enough access to deploy certain security features. Let’s say you would like to use security headers like Content Security Policy and Strict Transport Security, mange bot traffic, or do some advanced multi-factor authentication; that can be a struggle and consume many resources in your current infrastructure. With futuristic serverless platforms, such solutions can be deployed in seconds.`
+  },
+  {
+    key: "gillian-armstrong",
+    bio: "Gillian works as a Technical Lead in the Emerging Technologies unit in Liberty Mutual. Her team is focused on thinking about big problems, and working out how to solve them using innovative technology in interesting new ways. At the moment she is working on Artificial Intelligence, with a particular focus on Conversational AI design and development. She has more than a decade’s worth of experience in many technologies across the full stack, and loves being a software engineer as it allows her not just to think up big ideas, but also to make them a reality.",
+    photoUrl: "gillian-armstrong.jpg",
+    title: "Serverless and Chatbots: A Match Made in the Cloud",
+    speaker: "Gillian Armstrong",
+    abstract: `The Internet tutorials promised that building a chatbot was simple, and that the magic of Serverless would make it even simpler. And it was true. But it turns out that the simple runs out very quick. By the time I discovered this I was already hooked on both Conversational AI and Serverless technologies. Conversational Interfaces let users interact with technology in a new way - and Serverless lets developers build software in a new way. Combined they bring something brand new that requires both learning new skills and applying existing skills in new ways.\nOur team has built an Employee Digital Assistant for the employees of our Fortune 100 company. Using practical examples from our chatbot, let me share with you some of the lessons we learned that can get you started with your own chatbot. Building a simple chatbot is simple - building a great chatbot is what I’m going to show you how to do.`
+  },
+  {
+    key: "matthew-fortunka",
+    bio: "I have been at architect at the Admiral Group for the last 11 years with the majority of that time spent in the price comparison site, Confused.com. Since 2012 I have been immersed in Azure and responsible for migrating multiple projects to the cloud. In 2015 I started experimenting with Serverless technologies included Logic Apps, Azure Functions, Azure Web Apps and Azure Container Instances. 2018 has seen me present on Azure Integration technologies as a guest of Microsoft at their shows in Orlando and London.",
+    photoUrl: "matthew-fortunka.jpg",
+    title: "Azure Logic Apps - Serverless + codeless",
+    speaker: "Matthew Fortunka",
+    abstract: `Integration technologies are not the obvious first choice when looking to go Serverless. It is a space that is dominated by the old school monoliths with tools like biztalk and SSIS.\n * Learn how we used Azure Logic Apps to solve the holy grail of having a “living” workflow that could be read by both Product owners and developers.\n * See how to create a Logic app with little or no “code” that\n * Marvel at the diagnostics and reporting for every single invocation.`
+  },
+  {
+    key: "hendric-ruesch",
+    bio: "Hendric's journey has taken him from studying the social sciences to a deep fascination with the social web and digital strategy. He worked for Jimdo, SysEleven, strg. and as a freelance consultant. He grew up by the sea in Germany and lives in Hamburg. Hendric is the co-founder of superluminar, a cloud consulting firm and co-organiser of ServerlessDays Hamburg.",
+    photoUrl: "hendric-ruesch.jpg",
+    title: "Serverless Cheat Sheet for business discussions",
+    speaker: "Hendric Ruesch",
+    abstract: `We talked and worked with lots of companies in the last months and there are 2 types of people: the ones who say “hey, the future is here so let’s do serverless and get rid of the unnecessary stuff. The other ones are “we can only dream of serverless, please fix our containers first and then we can talk with our C[A-Z]O’s about serverless. We are convinced that more understanding of the business side (even though it looks often pretty obvious) is a crucial part. So, let’s work with a cheat sheet to speed things up and enable your organisation.`
+  },
+  {
+    key: "tom-wallace",
+    bio: "I trained as a bridge builder, cut my big data teeth on Formula 1 and nearly completed my management training in McDonalds. And whilst I rarely know what I’m doing, I’m always happy to share my experiences; in the hope that someone might put up their hand and finally set me in the right direction.",
+    photoUrl: "tom-wallace.jpg",
+    title: "Meta-programming in lambda- for bad and for worse",
+    speaker: "Tom Wallace",
+    abstract: `After taking a joke way too far, we ended up with the “single lambda application”. A monstrosity that would serve up an interface, respond to requests and edit its own deployment accordingly.\n From re-configuring timeouts all the way to updating the code itself- it was an experiment in just how far you can go meta-programming with lambda.\n And while our ‘single-lambda-app’ is unlikely to become the next big framework; how we got there sheds some light on tools that might just be useful.\n “They were so preoccupied with whether or not they could that they didn’t stop to think if they should"`
+  },
+  {
+    key: "simon-williams",
+    bio: "Simon is the DevOps Transformation Lead and Engineer at Capgemini, and has implemented a number of serverless applications for different clients.",
+    photoUrl: "simon-williams.jpg",
+    title: "Accelerating Business Delivery With Serverless",
+    speaker: "Simon Williams",
+    abstract: `A two month journey from inception to delivery for two websites, for a multi million pound business using serverless. Covering why serverless, challenges, organisational response, process change, development cycle, DevOps integration, traditional vs. serverless infrastructure and the final success.`
+  },
+  {
+    key: "liam-betsworth",
+    bio: "Liam is Head of User Experience at AMPLYFI.",
+    photoUrl: "liam-betsworth.jpg",
+    title: "Server or Serverless: The AMPLYFI Story",
+    speaker: "Liam Betsworth",
+    abstract: `AMPLYFI’s purpose is to fundamentally transform organisations’ decision making, business intelligence and research capabilities by enabling them to unlock the internet for themselves. They are tipped to be Wales’ first unicorn startup, and this year have already won the Wales 2018 Innovative Startup of the Year and the Spectator’s Economic Disruptor of the Year for the North West and Wales. Find out the real life experience of adopting serverless as a start up.`
+  },
 ];
